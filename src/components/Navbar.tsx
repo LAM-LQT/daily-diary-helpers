4ec1c0a837336459ca4bae7f9111cd21e4f5d5f3
lamquangthinh2001 <102337644+lamquangthinh2001@users.noreply.github.com>
2024-12-23 23:39:39 +1300

    change nav nbar title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
-            <h1 className="text-2xl font-bold text-purple-700">AI Diary</h1>
+            <h1 className="text-2xl font-bold text-purple-700">Thanks Everyday</h1>
           </div>
           
           {/* Desktop Navigation */}
